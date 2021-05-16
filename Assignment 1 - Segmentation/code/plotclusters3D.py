59542bc14cbb60f9984644ece87f7561dae1c8bb
kathrin229 <39700827+kathrin229@users.noreply.github.com>
2021-05-16 10:52:25 +0200

    segim function created

--- Assignment 1 - Segmentation/code/plotclusters3D.py
+++ Assignment 1 - Segmentation/code/plotclusters3D.py
@@ -75,9 +75,8 @@ def meanshift(data, r):
 
 # TODO List with labeled and unlabeled datapoints
 # first speedup: basin of attraction
-# TODO labels not working!!! -> commented code
 def meanshift_opt (data, r, c=4):
-    labels = np.empty(len(data))  # labels are numbers #TODO: fill with -1
+    labels = np.empty(len(data))  # labels are numbers, filled with -1
     labels.fill(-1)
     peaks = np.empty([0, 3])
     print(labels)
@@ -85,7 +84,6 @@ def meanshift_opt (data, r, c=4):
     label_peak = dict()
 
     label = 0  # has to be zero because of plot function
-    #for i, point in enumerate(data): # TODO: eliminate for loop here
     while True:
         checkCondition = np.where(labels == -1)[0]
         # print("size", i[0].size)
@@ -138,11 +136,15 @@ def find_peak_opt(data, idx, r, t = 0.01, c=4):
     peak_indexes = np.empty([0, 1])
 
     # find first mean at index position
+    # points = cdist(data, idx, metric='euclidean') < r
     distances = cdist(data, idx, metric='euclidean')
+    # indexes = np.where(distances < r)[0]
+    # points_in_circle = data[indexes]
     indexes = np.where(distances < r)
     for index in indexes[0]:
         points_in_circle = np.vstack([points_in_circle, data[index]])
     mean = np.mean(points_in_circle, axis=0)
+    # TODO: find surrounding points for first mean
 
     # TODO: for every mean, find surrounding points
     # array with points that have to be labeled
@@ -187,65 +189,49 @@ def plotclusters3D(data, labels, peaks):
     for idx, peak in enumerate(rgb_peaks):
         color = np.random.uniform(0, 1, 3)
         # TODO: instead of random color, you can use peaks when you work on actual images
-        color = peak
+        # color = peak
         cluster = data[np.where(labels == idx)[0]].T
         ax.scatter(cluster[0], cluster[1], cluster[2], c=[color], s=.5)
     print("showing figure")
     fig.show()
 
 
-# def segmIm(im, r):
+def segmIm(im, r):
+    img = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    plt.imshow(img)
+    plt.show()
 
+    # TODO: image preprocessing
+    # resize, blur, RGB to LAB
+    img = cv2.resize(img, (30, 40), interpolation=cv2.INTER_NEAREST)
+    img_original = img
+    img_width, img_height, _ = img.shape
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+    img = img.transpose(2, 0, 1).reshape(3, -1)
+    img = img.transpose()
 
-r = 40  # should give two clusters
+    labels, peaks = meanshift_opt(img, r)
+
+    x = 0
+    for i in range(img_width):
+        for j in range(img_height):
+            img_original[i][j] = peaks[int(labels[x])]
+            x += 1
+
+    img_resize = cv2.cvtColor(img_original, cv2.COLOR_LAB2RGB)
+    # img_resize = color.lab2rgb(img_resize)
+    plt.imshow(img_resize)
+    plt.show()
+
+r = 10  # 2 should give two clusters
 # labels, peaks = meanshift_opt(data, r)
 # labels, peaks = meanshift(data, r) # WORKS!
 # plotclusters3D(data, labels, peaks)
 # TODO: experiments - measure runtime?
 
+load_img = cv2.imread('../data/img-1.jpg')
+segmIm(load_img, r)
 
-# TODO: image preprocessing
-# resize, blur, RGB to LAB
-
-img = cv2.imread('../data/img-3.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
-img = cv2.cvtColor(img, cv2.COLOR_Lab2RGB)
-print(img[0][0])
-plt.imshow(img)
-plt.show()
-img_resize = cv2.resize(img, (30, 40), interpolation=cv2.INTER_NEAREST)
-img = cv2.resize(img, (30, 40), interpolation=cv2.INTER_NEAREST)
-plt.imshow(img)
-plt.show()
-img_height = 40 #len(img_resize[0])
-img_width = 30 #len(img_resize[1])
-img = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
-img_resize = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
-# img = color.rgb2lab(img)
-plt.imshow(img)
-plt.show()
-
-img = img.transpose(2,0,1).reshape(3,-1)
-img = img.transpose()
-
-labels, peaks = meanshift_opt(img, r)
-# peaks = peaks.reshape(len(peaks), 3, 1).transpose(0,2,1)
-# peaks = cv2.cvtColor(peaks, cv2.COLOR_LAB2RGB) TODO maybe ints solve problem
-# plotclusters3D(img, labels, peaks)
-
-# for idx in range(len(img)):
-#     img[idx] = peaks[int(labels[idx])]
-x = 0
-for i in range(img_height):
-    for j in range(img_width):
-        img_resize[i][j] = peaks[int(labels[x])]
-        x += 1
-
-# img = img.reshape(3, img_height, img_width).transpose(1, 2, 0)
-img_resize = cv2.cvtColor(img_resize, cv2.COLOR_LAB2RGB)
-# img_resize = color.lab2rgb(img_resize)
-plt.imshow(img_resize)
-plt.show()
-
-
+# speedup ideas
+# - where different usage
+# - list conversion to np array by np concatenate? instead of vstack
