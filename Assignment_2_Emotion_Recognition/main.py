f6712f1ab0723c0e45492cc8a5de7abea57395e3
Lena Cabrera <lena.m.cabrera@gmail.com>
2021-05-20 12:27:23 +0200

    Reshaped data and added one hot encoding for targets.

--- Assignment_2_Emotion_Recognition/main.py
+++ Assignment_2_Emotion_Recognition/main.py
@@ -4,39 +4,41 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
+
 data_cv = pd.read_csv('data/fer2013/fer2013/fer2013.csv').to_numpy()
 print(data_cv.shape)
 
-data = np.zeros((data_cv.shape[0], len(data_cv[0, 1].split(' ')) + 2))
-data[:, 0] = data_cv[:, 0]
-
+# convert "Training", "PrivateTest", "PublicTest" to ints
 unique_labels = np.unique(data_cv[:, 2])
 number_dict = dict(zip(unique_labels, range(len(unique_labels))))
 data_cv[:, 2] = np.array([number_dict[k] for k in data_cv[:, 2]])
+
+# convert string pixel values to ints
+data = np.zeros((data_cv.shape[0], len(data_cv[0, 1].split(' ')) + 2))
+data[:, 0] = data_cv[:, 0]
 data[:,  len(data_cv[0, 1].split(' ')) + 1] = data_cv[:, 2]
 data[:, 1:  len(data_cv[0, 1].split(' ')) + 1] = np.array([s.split(' ') for s in data_cv[:, 1]]).astype(int)
 
+# training, test, validation (x data shape: sample size, channel size, height, width)
+train = np.expand_dims(data[np.where(data[:, -1] == 2)], axis=2)  # Training
+x_train = torch.from_numpy(train[:, 1:-1, :])
+y_train_int = torch.from_numpy(train[:, 0, :])
+y_train = torch.nn.functional.one_hot(y_train_int[:, 0].to(torch.int64))
+
+test = np.expand_dims(data[np.where(data[:, -1] == 0)], axis=2)  # PrivateTest
+x_test = torch.from_numpy(test[:, 1:-1, :])
+y_test_int = torch.from_numpy(test[:, 0, :])
+y_test = torch.nn.functional.one_hot(y_test_int[:, 0].to(torch.int64))
+
+valid = np.expand_dims(data[np.where(data[:, -1] == 1)], axis=2)  # PublicTest
+x_valid = torch.from_numpy(valid[:, 1:-1, :])
+y_valid_int = torch.from_numpy(valid[:, 0, :])
+y_valid = torch.nn.functional.one_hot(y_valid_int[:, 0].to(torch.int64))
+
 # images: shape 48 x 48
-
-
-# training, test, validation
-train = data[np.where(data[:, -1] == 2)]  # Training
-x_train = torch.from_numpy(train[:, 1:-1])
-y_train = torch.from_numpy(train[:, 0])
-
 img = x_train[0].reshape(48, 48)
 plt.imshow(img, cmap='gray')
 plt.show()
-
-test = data[np.where(data[:, -1] == 0)]  # PrivateTest
-x_test = torch.from_numpy(test[:, 1:])
-y_test = torch.from_numpy(test[:, 0])
-
-valid = data[np.where(data[:, -1] == 1)]  # PublicTest
-x_valid = torch.from_numpy(valid[:, 1:])
-y_valid = torch.from_numpy(valid[:, 0])
-
-# data shape: sample size, channel size, height, width
 
 # input layer size, hidden layer size, output layer size and batch size (N)
 input_size = x_train.shape[1]
