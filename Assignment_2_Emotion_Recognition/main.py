339087f26a623fc1aa6760ef4637e8c79f0a3a36
Lena Cabrera <lena.m.cabrera@gmail.com>
2021-05-24 00:05:29 +0200

    Added early stopping.

--- Assignment_2_Emotion_Recognition/main.py
+++ Assignment_2_Emotion_Recognition/main.py
@@ -1,11 +1,12 @@
-import pandas as pd
 import numpy as np
+import copy
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import dataset
 from models import Conv1DNet, Conv2DNet
 from sklearn.metrics import precision_recall_fscore_support
+
 
 # TODO check seed - reproducibility
 torch.seed()
@@ -15,6 +16,7 @@ architecture = Conv1DNet
 num_epochs = 1
 learning_rate = 0.0001
 batch_size = 64
+patience = 15
 model_args = {
     'input_channel': 1,
 
@@ -40,9 +42,9 @@ print("Finished loading data.\n")
 
 if torch.cuda.is_available():
     device = torch.cuda.current_device()
-    print('Current device:', torch.cuda.get_device_name(device))
+    print('Current device:', torch.cuda.get_device_name(device), '\n')
 else:
-    print('Failed to find GPU. Will use CPU.')
+    print('Failed to find GPU. Will use CPU.\n')
     device = 'cpu'
 
 model = architecture(**model_args).to(device)
@@ -53,8 +55,12 @@ print(model)
 print("\n")
 
 min_valid_loss = np.inf
+stopping = 0
+max_val_acc = 0
+
+print("Fit model...")
 for epoch in range(num_epochs):
-    train_loss = 0.0
+    train_loss, train_correct, train_total = 0, 0, 0
     ######################################################
     # training loop (iterates over training batches)
     ######################################################
@@ -73,13 +79,17 @@ for epoch in range(num_epochs):
         optimizer.step()
         # accumulate the training loss
         train_loss += loss_train.item()
+        # calculate the accuracy
+        predicted = torch.argmax(y_pred, 1)
+        train_total += y_train.size(0)
+        train_correct += (predicted == y_train).sum().item()
 
     ######################################################
     # validation loop
     ######################################################
     # set the model to eval mode
     model.eval()
-    valid_loss = 0.0
+    valid_loss, valid_correct, valid_total = 0, 0, 0
     # turn off gradients for validation
     with torch.no_grad():
         for batch in valid_loader:
@@ -89,40 +99,63 @@ for epoch in range(num_epochs):
             y_pred = model(x_valid.float())
             # validation batch loss
             loss_valid = loss(y_pred, y_valid)
-            # accumulate the valid_loss
+            # accumulate the validation loss
             valid_loss += loss_valid.item()
+            # calculate the accuracy
+            predicted = torch.argmax(y_pred, 1)
+            valid_total += y_valid.size(0)
+            valid_correct += (predicted == y_valid).sum().item()
 
     # print epoch results
     train_loss /= len(train_loader)
     valid_loss /= len(valid_loader)
-    print(f'Epoch: {epoch+1}/{num_epochs}.. Training loss: {train_loss}.. Validation Loss: {valid_loss}')
+    train_accuracy = train_correct / len(train_loader)
+    valid_accuracy = valid_correct / len(valid_loader)
+    print(f'Epoch: {epoch+1}/{num_epochs}.. '
+          f'Training loss: {train_loss}.. Validation Loss: {valid_loss}'
+          f'Training accuracy: {train_accuracy}.. Validation accuracy: {valid_accuracy}')
+
+    # early stopping
+    if max_val_acc < valid_accuracy:
+        max_val_acc = valid_accuracy
+        weights = copy.deepcopy(model.state_dict())
+        stopping = 0
+    else:
+        stopping = stopping + 1
+
+    if stopping == patience:
+        print('Early stopping...')
+        print('Restoring best weights')
+        model.load_state_dict(weights)
+        break
+
 
 ######################################################
 # test loop
 ######################################################
-
+print("Test model...")
 # set the model to eval mode
 model.eval()
 # turn off gradients for validation
 with torch.no_grad():
-    correct = 0
-    total = 0
+    test_loss, test_correct, test_total = 0, 0, 0
     for batch in test_loader:
         x_test = batch[0].to(device)
         y_test = batch[1].to(device)
         # forward pass
         y_pred = model(x_test.float())
-        # validation batch loss
+        # test batch loss
         loss_test = loss(y_pred, y_test)
-        # accumulate the valid_loss
-        total += y_test.size(0)
+        # accumulate the test loss
+        test_loss += loss_test.item()
         # calculate the accuracy
         predicted = torch.argmax(y_pred, 1)
-        correct += (predicted == y_test).sum().item()
-    print('Test Accuracy: {}%'.format(100 * correct / total))
+        test_total += y_test.size(0)
+        test_correct += (predicted == y_test).sum().item()
+    print('Test Accuracy: {}%'.format(100 * test_correct / test_total))
 
-loss_test /= len(test_loader)
-accuracy = correct / len(test_loader)
+test_loss /= len(test_loader)
+accuracy = test_correct / len(test_loader)
 print(f'Test loss: {loss_test}.. Test Accuracy: {accuracy}')
 
 precision, recall, fscore, support = precision_recall_fscore_support(y_valid, predicted, average='macro')
